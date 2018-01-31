d4711eb0dd5411d5fa1f71bc9f08136e6f5971d7
Isacco <Isacco@ISACCO-PC>
2018-01-31 19:10:52 +0100

    modifica

---
+++ Somma01/ClassSomma.txt/ClassSomma.txt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSomma.txt
+{
+    public class Calc
+    {
+        static int Somma1 (int a, int b)
+        {
+            int somma = 0;
+            somma = a + b;
+            return somma;
+        }
+    }
+}
---
+++ Somma01/MathLibrary.txts/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathLibrary.txts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathLibrary.txts")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ae60886-b511-4725-b0bd-9c9475e8cc12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
